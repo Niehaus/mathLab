bd38c308079b2edd1b5f313e4ad819c3a5d5d75c
Bárbara Boechat <barbara_belize@hotmail.com>
2019-04-30 01:15:15 -0300

    New Features
    Novas Cartas + suas combinações na tela de demeonstração das possiveis combinações

---
+++ mathLab/Assets/Scripts/menuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public void triggerMenu(int trigger) {
+		switch (trigger) {
+		case(0) :
+			SceneManager.LoadScene("gameScene");
+			break;
+		case(1) :
+			Application.Quit();
+			break;
+		}
+	}
+
+    public void showCardsMenu(int trigger)
+    {
+        switch (trigger)
+        {
+            case (0):
+                SceneManager.LoadScene("Inferencia - 4 cartas");
+                break;
+            case (1):
+                Application.Quit();
+                break;
+        }
+    }
+}

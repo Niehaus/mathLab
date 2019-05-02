9f51b3b0b5f7549adcc55ee47e988158c0c96d0a
Bárbara Boechat <barbara_belize@hotmail.com>
2019-05-02 20:45:45 -0300

    +1
    Cartas e Combinações c animação pra discover de cada carta!

---
+++ mathLab/Assets/Scripts/flipCardMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class flipCardMenu : MonoBehaviour
+{
+    [SerializeField]
+    private int flip;
+
+    public Sprite cardDown;
+    public Sprite cardFace;
+
+    public Image imagem_atual;
+
+    private void Start() {
+        flip = 1;
+    }
+    public void flippy() {
+        if (flip == 0) {
+            flip = 1;
+        } else if (flip == 1) {
+            flip = 0;
+        }
+
+        if (flip == 0)
+            imagem_atual.sprite = cardFace;
+        else if (flip == 1)
+            imagem_atual.sprite = cardDown;
+    }
+}

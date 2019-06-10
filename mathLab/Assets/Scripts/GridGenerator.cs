e654f387e996accca979501fb7d3e1ecc1a74e86
Bárbara Boechat <barbara_belize@hotmail.com>
2019-06-09 21:44:12 -0300

    aba de config feita, inicio de add tabelas
    Inicio de como adicionar novas tabelas verdade, ainda não sei bem como fazer com o que o padrão estabelecido receba algo novo e prossiga com o jogo daí, talvez um vetor de scenes seja uma solução

--- mathLab/Assets/Scripts/GridGenerator.cs
+++ mathLab/Assets/Scripts/GridGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+
 public class GridGenerator : MonoBehaviour {
     private GridTile[,] grid;
 
---
+++ mathLab/Assets/Scripts/configMaster.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class configMaster : MonoBehaviour {
+
+    public GameObject audioPanel, prefPanel,truthPanel, equivPanel, infPanel;
+    
+    public void openPanel(int panelNum) {
+         
+         switch (panelNum) {
+            case (0):
+                audioPanel.SetActive(true);
+                break;
+            case (1):
+                prefPanel.SetActive(true);
+                break;
+            case (2):
+                truthPanel.SetActive(true);
+                equivPanel.SetActive(false);
+                infPanel.SetActive(false);
+                break;
+            case (3): 
+                equivPanel.SetActive(true);
+                infPanel.SetActive(false);
+                truthPanel.SetActive(false);
+                break;
+            case (4): 
+                infPanel.SetActive(true);
+                equivPanel.SetActive(false);
+                truthPanel.SetActive(false);
+                break;
+        }
+        
+    }
+    
+    public void closePanel(int panelNum) {
+        
+            switch (panelNum) {
+                case (0):
+                    audioPanel.SetActive(false);
+                    break;
+                case (1):
+                    prefPanel.SetActive(false);
+                    break;
+                case (2):
+                    truthPanel.SetActive(false);
+                    break;
+                case (3): 
+                    equivPanel.SetActive(false);
+                    break;
+                case (4): 
+                    infPanel.SetActive(false);
+                    break;
+            }
+    }
+
+}

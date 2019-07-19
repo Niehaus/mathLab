abe6e31ff2aca59390f807e67c9306c2905429aa
Bárbara Boechat <barbara_belize@hotmail.com>
2019-07-19 15:31:38 -0300

    bug ao fazer coluna dos negativos quando p ^ t || -t
    - resolver qd chegar

--- mathLab/Assets/Scripts/setAddTables.cs
+++ mathLab/Assets/Scripts/setAddTables.cs
@@ -8,13 +8,12 @@ public class setAddTables : MonoBehaviour {
     public Text[] colunas;
     public Text expressao;
     protected string pathTable = "Assets/NewAdds/tabelaVerdade.txt", line;
-    private string[] resp, expr;
-    protected string[] retornaArray = new String[32];
-    private int tableNumber = 0, numLinhas;
-    private Hashtable convertColunas = new Hashtable();
+    protected string[] retornaArray = new String[32], resp, expr;
+    private int tableNumber = 0, numLinhas, count = 0;
     static int counter = 0;
-    // Ler o arquivo e pegar a tabela - setar o ponteiro no arquivo pra ler a prox tabela 
-    void Start() {
+    private Hashtable convertColunas = new Hashtable();
+    
+    void Start() { // Ler o arquivo e pegar a tabela - setar o ponteiro no arquivo pra ler a prox tabela 
         //colunas positivas
         convertColunas.Add("p",0);
         convertColunas.Add("q",1);
@@ -39,12 +38,6 @@ public class setAddTables : MonoBehaviour {
                     line = file.ReadLine(); //pega expressao
                     expressao.text = line;
                     expr = splitString(line); //separa expr pra associar uma coluna da tabela a uma variavel
-                    foreach (var item in expr) {
-                        if (item != null) {
-                            Debug.Log("var " + item);    
-                            Debug.Log("colunas " + item + "," + convertColunas[item]);
-                        }
-                    }
                     line = file.ReadLine(); //pega num de linhas da tabela
                     numLinhas = convert(line[0]);
                     Debug.Log(numLinhas);
@@ -53,7 +46,28 @@ public class setAddTables : MonoBehaviour {
                     Debug.Log("Linhas da tabela");
                     for (int i = 0; i < TotalDeLinhas; i++) { //a partir da conta do total de linhas pega tdas as linhas
                         Debug.Log(line = file.ReadLine());
-                        Debug.Log("primeiro item da coluna " + line[0]);
+                        foreach (var item in expr) { //associa cada coluna da tabela a uma variavel e printa na tela
+                            if (item != null) {
+                                Debug.Log("ITEM = " + item);
+                                if (item.Contains("-")) {
+                                    Debug.Log("ITEM2 = " + item + "code: " +  convertColunas[item].GetHashCode()) ;
+                                    Debug.Log("Code positivo: " + (convertColunas[item].GetHashCode() - 5));
+                                    Debug.Log("Pos linha: " + count + "linha: " + line[count]);
+                                    colunas[(convertColunas[item].GetHashCode() - 5)].text += line[count] + "\n"; //escreve a linha da tabela no postivo
+                                    if (line[count] == 'V') { // nega a linha da tabela e escreve no negativo    
+                                        Debug.Log("entra aqui?");
+                                        colunas[convertColunas[item].GetHashCode()].text += 'F' + "\n"; 
+                                    }else {
+                                        colunas[convertColunas[item].GetHashCode()].text += 'V' + "\n";
+                                    }
+                                    count += 2;
+                                }else {
+                                    colunas[convertColunas[item].GetHashCode()].text += line[count] + "\n";
+                                    count += 2;    
+                                }
+                            }
+                        }
+                        count = 0;
                     }
                     file.ReadLine(); //lê o espaço entre a tabela e as respostas
                     line = file.ReadLine();
@@ -72,19 +86,16 @@ public class setAddTables : MonoBehaviour {
             Debug.Log("não tem prox tabela");
         }
     }
-    public int convert(char numVar){
+    public int convert(char numVar) {
         int convertido = (int)Char.GetNumericValue(numVar);
         return convertido;
     }
 
-    public String[] splitString(string expr)
-    {  //separa a string a partir dos operadores - get variaveis
+    public String[] splitString(string expr) {  //separa a string a partir dos operadores - get variaveis
         int k = 0;
         string[] multiArray = expr.Split(new Char[] { ' ', '^', '|', '(', ')', '[', ']' });
         foreach (string author in multiArray) {
-            if (author.Trim() != "")
-            {
-                //Debug.Log(k + " " + author);
+            if (author.Trim() != "") {
                 retornaArray[k] = author;
                 k++;
             }

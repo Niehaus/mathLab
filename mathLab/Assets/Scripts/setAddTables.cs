6b65e2f1ee39cde121a990d0ab317728562b5a5f
Bárbara Boechat <barbara_belize@hotmail.com>
2019-08-03 21:41:03 -0300

    inputs associados e funcionando, get e set é top

--- mathLab/Assets/Scripts/setAddTables.cs
+++ mathLab/Assets/Scripts/setAddTables.cs
@@ -8,15 +8,18 @@ using System;
 public class setAddTables : MonoBehaviour { 
     public Text[] colunas;
     public Text expressao;
+    public InputField inputPrefab;
+    public InputField[] inputs;
+    public GameObject endGame;
     protected string pathTable = "Assets/NewAdds/tabelaVerdade.txt", line, resps;
     protected string[] resp, expr;
     private int tableNumber = 0, numLinhas, count = 0, vars = 0;
-    static int counter = 0;
     private Hashtable convertColunas = new Hashtable();
-    public InputField inputPrefab;
-    public InputField[] inputs;
     private Transform ResultadoTransform;
+    private int[] acertos;
+    static int counter = 0, certo;
     
+  //  private GameObject inputScript;
     void Start() { // Ler o arquivo e pegar a tabela - setar o ponteiro no arquivo pra ler a prox tabela 
         //colunas positivas
         convertColunas.Add("p",0);
@@ -32,7 +35,7 @@ public class setAddTables : MonoBehaviour {
         convertColunas.Add("-t", 9);
         //transform p/ setar inputs como Childs de Respostas na hierarquia 
         ResultadoTransform = GameObject.FindWithTag("Respostas").transform;
-        inputs =
+        //inputs =
     }
     public void setaTabela() {
         System.IO.StreamReader file = new System.IO.StreamReader(pathTable);
@@ -53,15 +56,14 @@ public class setAddTables : MonoBehaviour {
                     Debug.Log(numLinhas);
                     Debug.Log(expr);
                     double TotalDeLinhas = Math.Pow(2, Convert.ToDouble(numLinhas));
+                    acertos = new int[Convert.ToInt32(TotalDeLinhas)];
                     Debug.Log("Linhas da tabela");
                     for (int i = 0; i < TotalDeLinhas; i++) { //a partir da conta do total de linhas pega tdas as linhas
-                        Debug.Log(line = file.ReadLine());
-                        inputs[i] = Instantiate(inputPrefab, new Vector3(0, 194.7f - (46f * i), 0), Quaternion.identity);
+                        line = file.ReadLine(); //linha da tabela
+                        inputs[i] = Instantiate(inputPrefab, new Vector3(0, 194.7f - (46f * i), 0), Quaternion.identity); //seta novo input em sua coordenada
                         inputs[i].transform.SetParent(ResultadoTransform, false); //seta como child de Resultados
-                        //inputs[i].onEndEdit.AddListener(delegate { verificaInput(i); });
-                        inputs[i].id.set(i);
-                        Debug.Log("VALOR DO INPUT" + inputs[i].id.get());
-                        inputs[i].text = i.ToString();
+                        setId inputScript = inputs[i].GetComponent<setId>();
+                        inputScript.Id = i;
                         Debug.Log("NUMB DE VARS " + vars);
                         Debug.Log("numLinhas " + numLinhas);
                         if (vars > numLinhas) {
@@ -70,11 +72,11 @@ public class setAddTables : MonoBehaviour {
                         }else {
                             foreach (var item in expr) { //associa cada coluna da tabela a uma variavel e printa na tela
                                 if (item != null) {
-                                    Debug.Log("ITEM aqui = " + item);
-                                    Debug.Log("+COUNT VALUE " + count + "i value " + i);
+                                    //Debug.Log("ITEM aqui = " + item);
+                                    //Debug.Log("+COUNT VALUE " + count + "i value " + i);
                                     if (item.Contains("-")) {
-                                        Debug.Log("ITEM2 = " + item + "code: " + convertColunas[item].GetHashCode());
-                                        Debug.Log("-COUNT VALUE" + count);
+                                        //Debug.Log("ITEM2 = " + item + "code: " + convertColunas[item].GetHashCode());
+                                        //Debug.Log("-COUNT VALUE" + count);
                                         colunas[(convertColunas[item].GetHashCode() - 5)].text += line[count] + "\n"; //escreve a linha da tabela no postivo
                                         if (line[count] == 'V') { // nega a linha da tabela e escreve no negativo    
                                             colunas[convertColunas[item].GetHashCode()].text += 'F' + "\n";
@@ -96,11 +98,6 @@ public class setAddTables : MonoBehaviour {
                     file.ReadLine(); //lê o espaço entre a tabela e as respostas
                     resps = file.ReadLine();
                     resp = splitString(resps); //separa o vetor de respostas p associar aos inputs
-                    foreach (var item in resp) {
-                        if (item != null) {
-                        //    Debug.Log(item);
-                        }
-                    }
                     counter++;
                     break;
                 }
@@ -115,7 +112,7 @@ public class setAddTables : MonoBehaviour {
         return convertido;
     }
 
-    public String[] splitString(string expr) {  //separa a string a partir dos operadores - get variaveis
+    public String[] splitString(string expr) { //separa a string a partir dos operadores - get variaveis
         int k = 0;
         vars = 0;
         string[] retornaArray = new String[32];
@@ -130,9 +127,39 @@ public class setAddTables : MonoBehaviour {
         return retornaArray;
     }
 
-    public void verificaInput(){
-          
-        Debug.Log("input num = ");
-        //Debug.Log("texto do input 0 " + inputs.text);
+    public void verificaInput(InputField input) {
+        setId inputAtual = input.GetComponent<setId>();
+        Debug.Log("MEU ID É = " + inputAtual.Id);
+        // Debug.Log("entrada: " + inputs[inputAtual.Id].text); Debug.Log("reposta: " + resp[inputAtual.Id]); 
+        if (resp[inputAtual.Id].Equals(inputs[inputAtual.Id].text)) {
+            //Debug.Log("resposta certa 1");
+            inputs[inputAtual.Id].image.color = new Color32(69, 202, 35, 255);
+            acertos[inputAtual.Id] = 1;
+        }else {
+            //Debug.Log("resposta errada");
+            inputs[inputAtual.Id].image.color = new Color32(202, 41, 49, 255);
+            acertos[inputAtual.Id] = 0;
+        }
+        verficaAcertos();
     }
+
+    public void verficaAcertos() {
+        certo = 0;
+        for (int i = 0; i <= acertos.Length; i++) {
+            Debug.Log("indice = " + i);
+            if (acertos[i] == 1) {
+                certo += 1;
+                Debug.Log("acerto: " + certo);
+                if (certo == acertos.Length)
+                {
+                    Debug.Log("fim de jogo");
+                    endGame.SetActive(true);
+                }
+            }else {
+                Debug.Log("Ainda não acabou");
+                break;
+            }
+        }
+    }
+
 }
--- mathLab/Assets/Scripts/setId.cs
+++ mathLab/Assets/Scripts/setId.cs
@@ -1,15 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class setId : MonoBehaviour
-{
+public class setId : MonoBehaviour {
+    
     private int id;
     public int Id { get; set; }
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-    
 
+    
 }

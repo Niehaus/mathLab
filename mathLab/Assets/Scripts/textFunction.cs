b0a89a00832aa944fb386e9538fa9af7444eca3d
Bárbara Boechat <barbara_belize@hotmail.com>
2019-09-10 13:51:56 -0300

    arrumar ponteiro do arquivo

--- mathLab/Assets/Scripts/textFunction.cs
+++ mathLab/Assets/Scripts/textFunction.cs
@@ -1,34 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class textFunction : MonoBehaviour {
 
     public Text textoGame;
-    protected string pathTableEqvLog = "Assets/NewAdds/eqvLogica.txt", eqvLogica;
+    protected string pathTableEqvLog = "Assets/NewAdds/eqvLogica.txt", eqvLogica,resp;
+    private Vector3 posInicial;
     void Start() {
         textoGame.text = getEqvLog();
+        posInicial = textoGame.transform.position;
     }
 
    void OnTriggerEnter2D(Collider2D other) {
     if (other.CompareTag("EqvLog")) {
         Debug.Log("EQV LOG AQUI");
-        textoGame.transform.position = new Vector3(0f, 0f, 0f);
+        textoGame.transform.position = posInicial;
+        textoGame.text = getEqvLog();
+       /*posiçao resetada porem arquivo tambem sempre reseta o ponteiro precisa arrumar isso */
     }       
    }
 
    public string getEqvLog(){
         System.IO.StreamReader file = new System.IO.StreamReader(pathTableEqvLog);
-      
-        if (file.ReadLine() == "NOVA EQV LOGICA")
-        {
+        if (file.ReadLine() == "NOVA EQV LOGICA") {
             Debug.Log("Nova eqv logica aqui");
             Debug.Log(file.ReadLine());
             eqvLogica = file.ReadLine();
             Debug.Log("expr = " + eqvLogica);
+            resp = file.ReadLine();
+            Debug.Log("resp = " + resp);
+        }else{
+            Debug.Log("linha vazia?" + file.ReadLine());
+            Debug.Log("nova eqv??" + file.ReadLine());
         }
-
        return eqvLogica;
    }
 }

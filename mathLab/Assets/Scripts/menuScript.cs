09f24a63b5cd596626c402412b4849120e7498e3
Bárbara Boechat <barbara_belize@hotmail.com>
2019-10-05 17:22:44 -0300

    inicinado tutorial -
    necessrio planejar as falas e o tutorial

--- mathLab/Assets/Scripts/menuScript.cs
+++ mathLab/Assets/Scripts/menuScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 
 public class menuScript : MonoBehaviour {
+
     public void triggerMenu(int trigger) {
 		switch (trigger) {
             case (0):
@@ -39,6 +40,23 @@ public class menuScript : MonoBehaviour {
                 break;
             case (1):
                 Application.Quit();
+                break;
+        }
+    }
+
+    public void tutorialskip(int op){
+        GameObject panel = GameObject.FindGameObjectWithTag("TutoPanel");
+        GameObject panelTalk = GameObject.FindGameObjectWithTag("TutoPanelTalk");
+        GameObject manager = GameObject.FindGameObjectWithTag("manager");
+        switch (op) {
+            case (0):
+                SceneManager.LoadScene("Jogo - Tabela Verdade Next (2)");
+                break;
+            case(1):
+                panel.SetActive(false);
+                panelTalk.SetActive(true);
+                tutorial man = manager.GetComponent<tutorial>();
+                man.iniciaTutorial();
                 break;
         }
     }
---
+++ mathLab/Assets/Scripts/tutorial.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    public GameObject instrutor;
+    public void iniciaTutorial(){
+        Instantiate(instrutor, new Vector3(161.5373f, 161.5373f, 1f), Quaternion.identity);
+
+    }
+}

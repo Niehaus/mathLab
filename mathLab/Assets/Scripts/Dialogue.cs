36afbcfa22efdb5d3fd2a5490b64f39bc06da6ad
Bárbara Boechat <barbara_belize@hotmail.com>
2019-10-12 18:31:48 -0300

    tutorial bem encaminhado -> terminar

--- mathLab/Assets/Scripts/Dialogue.cs
+++ mathLab/Assets/Scripts/Dialogue.cs
@@ -1,14 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-[System.Serializable]
-public class Dialogue {
-
-    public string name;
-
-    [TextArea(3,10)]
-    public string[] sentences;
-
+public class Dialogue : MonoBehaviour {
+   public TextMeshProUGUI textDisplay;
+   public string[] sentences;
+   private int index;
+   public float typingSpeed;
+   public GameObject continueButton;
+   public GameObject[] panelsActive;
    
+   public Animator instrutor;
+   void Start() {
+       StartCoroutine(Type());
+   }
+   void Update()    {
+       if (textDisplay.text == sentences[index]) {
+            instrutor.SetBool("fala", false);
+           continueButton.SetActive(true);
+       }
+       if (index == 4) {
+           panelsActive[0].SetActive(true);
+           panelsActive[1].SetActive(true);
+       }else {
+            panelsActive[0].SetActive(false);
+            panelsActive[1].SetActive(false);
+       }
+   }
+    IEnumerator Type(){
+        foreach (char letter in sentences[index].ToCharArray()) {
+            textDisplay.text += letter;
+            instrutor.SetBool("fala",true);
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence(){
+        continueButton.SetActive(false);
+        if (index < sentences.Length - 1) {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }else {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+
 }

6cb5e711780cc4d55728d73a26128da34d2a49a2
Bárbara Boechat <barbara_belize@hotmail.com>
2019-11-12 21:36:41 -0300

    camera max,min att

--- mathLab/Assets/Scripts/Jogo - Inferencia/CameraController.cs
+++ mathLab/Assets/Scripts/Jogo - Inferencia/CameraController.cs
@@ -6,16 +6,27 @@ public class CameraController : MonoBehaviour
 {
 
     public Transform target;
+    public float smoothing; 
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);   
+    // Update is called once per frame after Update function
+    void LateUpdate() {
+        //transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);   acompanha o player
+        
+        if (transform.position != target.position) { //dá um certo smooth movement à câmera, a faz andar atrás do player, e colocá-la sobre ele quando ele parar de andar(followup)
+            
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x); //pega um valor minimo e maximo e retorna um outro valor entre ambos
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
     }
 }

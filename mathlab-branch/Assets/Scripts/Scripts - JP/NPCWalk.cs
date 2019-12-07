5d38ad069da5f35d2d92eeba54ac3039230c0a3b
Bárbara Boechat <barbara_belize@hotmail.com>
2019-12-06 23:09:46 -0300

    att dr.wood
    animações dr.wood e scene casa dr.wood -> prox fase interações + vídeo

---
+++ mathlab-branch/Assets/Scripts/Scripts - JP/NPCWalk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCWalk : MonoBehaviour {
+
+    private Rigidbody2D myRB;
+    private Animator myAnim;
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start() {
+        myRB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()     {
+        myRB.velocity = new Vector2(0.2f, 0.2f).normalized * speed * Time.deltaTime;
+        myAnim.setFloat("moveX",myRB.velocity.x);
+        myAnim.setFloat("moveY",myRB.velocity.y);
+        
+    }
+}

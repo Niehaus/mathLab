292e0364c18abd1efb5e23086daf36a8a42b33bd
Bárbara Boechat <barbara_belize@hotmail.com>
2020-01-16 13:58:36 -0300

    gerando itens em ambas as areas

--- mathlab-branch/Assets/Scripts/Scripts - Jogo Eqv Log/Manager.cs
+++ mathlab-branch/Assets/Scripts/Scripts - Jogo Eqv Log/Manager.cs
@@ -8,6 +8,7 @@ using Random = System.Random;
 public class Manager : MonoBehaviour {
 
      public Text[] contador;
+
      internal static int _coins;
      internal static int _hearts = 3;
      
@@ -18,11 +19,13 @@ public class Manager : MonoBehaviour {
      private Cenoura _cenoura;
      private Coins _coins1;
      private Pao _pao;
-     Random rand = new Random(); 
+     Random rand = new Random();
+     
      
      // Start is called before the first frame update
     void Start() {
-        Debug.Log(rand.NextDouble() * 10) ;
+        
+        Debug.Log("go") ;
         _coins1 = FindObjectOfType<Coins>();
         _cenoura = FindObjectOfType<Cenoura>();
         _pao = FindObjectOfType<Pao>();
@@ -30,13 +33,15 @@ public class Manager : MonoBehaviour {
         contador[0].text = _coins + "x";
         contador[1].text =  _hearts + "x" ;
         
+       
         for (int i = 1; i < qnt_Cenouras + 1; i++) {
-            Debug.Log("time" + (i * _cenoura.spawn));
-            Debug.Log("time" + (i * _pao.spawn));
-            Debug.Log("time" + (i * _coins1.spawn));
-            StartCoroutine(Spawn(i * _cenoura.spawn, _cenoura.transform));    
-            StartCoroutine(Spawn(i * _pao.spawn, _pao.transform));    
-            StartCoroutine(Spawn(i * _coins1.spawn, _coins1.transform));    
+            StartCoroutine(Spawn(i * _cenoura.spawn, _cenoura.transform, rand.NextDouble()));
+        }
+        for (int i = 1; i < qnt_Coins + 1; i++) {
+            StartCoroutine(Spawn(i * _coins1.spawn, _coins1.transform, rand.NextDouble()));
+        }
+        for (int i = 1; i < qnt_Hearts + 1; i++) {
+            StartCoroutine(Spawn(i * _pao.spawn, _pao.transform, rand.NextDouble()));
         }
        
         
@@ -47,15 +52,41 @@ public class Manager : MonoBehaviour {
         
     }
 
-    private IEnumerator Spawn(int time, Transform prefab) {
-        
+    private IEnumerator Spawn(int time, Transform prefab, double orientacao) {
         yield return new WaitForSeconds(time);
-        
-        Instantiate(prefab, new Vector3((float) (rand.NextDouble() * 20.0f), 0, 0), Quaternion.identity);
-        
+        if (orientacao > 0.5) { //direita
+            Debug.Log("Direita");
+            var vector = GeraCoord(new Vector2(9.3f, -4.3f), new Vector2(3.23f, 5.36f));
+            Instantiate(prefab, new Vector3(vector.x, vector.y, 0), Quaternion.identity);
+        }
+        else { //esquerda
+            Debug.Log("Esquerda");
+            var vector = GeraCoord(new Vector2(-8.4f, -4.3f), new Vector2(-2.16f, 5.36f));
+            Instantiate(prefab, new Vector3(-vector.x, vector.y, 0), Quaternion.identity);
+        }
         Debug.Log("ok");
-        
-        
     }
+
+    //TODO: Tentar usar uma função só.. talvez valores absolutos sejam utéis aqui --> RESOLVIDO!
+    private Vector2 GeraCoord(Vector2 minAltura, Vector2 minLargura) {
+        
+        var valorX =  (rand.NextDouble() + 10 * rand.NextDouble()) + Math.Abs(minLargura.x);
+        var valorY =  rand.NextDouble() + 10 * rand.NextDouble() + minAltura.y;    
+            
+        Debug.Log(valorX);
+        while (Math.Abs(valorX) > Math.Abs(minAltura.x)) {
+            valorX = (rand.NextDouble() + 10 * rand.NextDouble()) + Math.Abs(minLargura.x);
+            Debug.Log("ERROU - Novo valor em X: " + valorX);
+        }
+        
+        while (valorY > minLargura.y) {
+            valorY =  rand.NextDouble() + 10 * rand.NextDouble() + minAltura.y;
+            Debug.Log("ERROU - Novo valor em Y: " + valorY);
+        }
+        var coordenadas = new Vector2((float) valorX, (float) valorY);
+        return coordenadas;
+    }
+    
+    
 
 }

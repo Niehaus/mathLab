83349d4003090d99c4a7b7b3af64f0b86a68562c
Bárbara Boechat <barbara_belize@hotmail.com>
2020-09-17 23:56:35 -0300

    att fase 1 -> formar uma lista com os switches ativos para pegar a linha

---
+++ mathlab-branch/Assets/Scripts/Scripts - Nova Fase 1 TB/Switch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch : MonoBehaviour {
+    
+    public Text textoTv;
+    public Text variavel_ref;
+    private Animator _myAnim; 
+    private bool _playerInRange;
+
+    private static readonly int On = Animator.StringToHash("on");
+
+    // Start is called before the first frame update
+    void Start() {
+        _myAnim = GetComponent<Animator>();
+    }
+    
+    
+    private void Update() {
+        if (!_playerInRange || !Input.GetKeyDown(KeyCode.Space)) return;
+        _myAnim.SetBool(On, !_myAnim.GetBool(On));
+        
+        if (_myAnim.GetBool(On)) {
+            textoTv.text = "V";    
+        }
+        else {
+            textoTv.text = "F";
+        }
+
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            _playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (!other.CompareTag("Player")) return;
+        _playerInRange = false;
+    }
+}
---
+++ mathlab-branch/Assets/Scripts/Scripts - Nova Fase 1 TB/TabVerdadeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabVerdadeManager : MonoBehaviour {
+    
+    public Switch[] switchVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var validSwitch in switchVector) {
+            if (validSwitch.GetComponent<Animator>().GetBool("on")) {
+                Debug.Log("switch ativo " + validSwitch.variavel_ref);
+            }
+        }
+    }
+}

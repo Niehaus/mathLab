11a14d73d67a3e99f7a7f60e0573b862854b4fe1
Bárbara Boechat <barbara_belize@hotmail.com>
2020-09-19 00:18:50 -0300

    fase 1 algoritmo quase ok

--- mathlab-branch/Assets/Scripts/Scripts - Nova Fase 1 TB/Switch.cs
+++ mathlab-branch/Assets/Scripts/Scripts - Nova Fase 1 TB/Switch.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,11 +7,17 @@ using UnityEngine.UI;
 public class Switch : MonoBehaviour {
     
     public Text textoTv;
-    public Text variavel_ref;
+    public Text variavelRef;
+    public GameObject conjuntoTV;
+    [NonSerialized] public string minhaResposta;
     private Animator _myAnim; 
     private bool _playerInRange;
-
+    public bool switchAtivo = true;
+    
     private static readonly int On = Animator.StringToHash("on");
+    private static readonly int Colorful = Animator.StringToHash("colorful");
+    private static readonly int GreenScreen = Animator.StringToHash("greenScreen");
+    private static readonly int RedScreen = Animator.StringToHash("redScreen");
 
     // Start is called before the first frame update
     void Start() {
@@ -21,7 +28,6 @@ public class Switch : MonoBehaviour {
     private void Update() {
         if (!_playerInRange || !Input.GetKeyDown(KeyCode.Space)) return;
         _myAnim.SetBool(On, !_myAnim.GetBool(On));
-        
         if (_myAnim.GetBool(On)) {
             textoTv.text = "V";    
         }
@@ -29,7 +35,9 @@ public class Switch : MonoBehaviour {
             textoTv.text = "F";
         }
 
-        
+        if (!switchAtivo) {
+            _myAnim.SetBool(On, false);
+        }
     }
     
     private void OnTriggerEnter2D(Collider2D other) {
@@ -41,5 +49,10 @@ public class Switch : MonoBehaviour {
     private void OnTriggerExit2D(Collider2D other) {
         if (!other.CompareTag("Player")) return;
         _playerInRange = false;
+    }
+
+    public void ConjuntoSwitchAction(string instrucao, bool conjuntoTVAction, bool textoTVAction) {
+        conjuntoTV.GetComponent<Animator>().SetBool(instrucao, conjuntoTVAction);
+        textoTv.gameObject.SetActive(textoTVAction);
     }
 }
--- mathlab-branch/Assets/Scripts/Scripts - Nova Fase 1 TB/TabVerdadeManager.cs
+++ mathlab-branch/Assets/Scripts/Scripts - Nova Fase 1 TB/TabVerdadeManager.cs
@@ -1,23 +1,169 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TabVerdadeManager : MonoBehaviour {
-    
+
+    public Text expressaoText;
+    public Text logLinhas;
     public Switch[] switchVector;
+    private TextAsset[] _textFiles;
+    private int _vars;
+    private List<Tuple<string, string>> _tuplaDeLinhasRespostas = new List<Tuple<string, string>>();
+    private List<Tuple<string, string>> _logRespostas = new List<Tuple<string, string>>();
+    private List<Switch> _switchesValidos = new List<Switch>();
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        _textFiles = Resources.LoadAll("TabVerdade", typeof(TextAsset)).Cast<TextAsset>().ToArray();
+        foreach (var textAsset in _textFiles) {
+            IniciaEtapa(textAsset);
+            
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (var validSwitch in switchVector) {
-            if (validSwitch.GetComponent<Animator>().GetBool("on")) {
-                Debug.Log("switch ativo " + validSwitch.variavel_ref);
+        
+    }
+
+    public void IniciaEtapa(TextAsset textAsset) {
+        var infoTable = GetLinesFromTable(textAsset);
+        expressaoText.text = infoTable.Item2;
+        _switchesValidos =  ValidaSwitches(infoTable.Item2);
+        for (var i = 0; i < infoTable.Item1.Length; i++) {
+            _tuplaDeLinhasRespostas.Add(LinhasRespostas(infoTable.Item1[i], infoTable.Item3[i]));
+        }
+        foreach (var tuple in _tuplaDeLinhasRespostas) {
+            Debug.Log(tuple);
+        }
+    }
+
+    private Tuple<string, string> LinhasRespostas(string linha, string resposta) {
+        return new Tuple<string, string>(linha, resposta);
+    }
+    
+    /* Valida apenas os switches que estão contidos
+    * na expressão avaliada naquele momento
+    */
+    private List<Switch> ValidaSwitches(string expressao) { 
+        var expressaoSeparada = SplitString(expressao);
+        var switchesValidos  = new List<Switch>();
+        
+        foreach (var @switch in switchVector) {
+            if (!expressaoSeparada.Contains(@switch.variavelRef.text.ToLower())  && @switch.variavelRef.text != "Resposta") {
+                @switch.switchAtivo = false;
+                @switch.ConjuntoSwitchAction("colorful", true, false);
+                
+                //TODO: desabilitar switch anim
+            }
+            else {
+                switchesValidos.Add(@switch);   
             }
         }
+        /* foreach (var @valido in _switchesValidos) {
+             Debug.Log(@valido.variavelRef);
+         }*/
+       return switchesValidos;
+    }
+
+    private String[] SplitString(string expr) { //separa a string a partir dos operadores - get variaveis
+        var k = 0;
+        _vars = 0;
+        var retornaArray = new String[32];
+        var multiArray = expr.Split(new Char[] { ' ', '^', '|', '(', ')', '[', ']'});
+        foreach (string author in multiArray) {
+            if (author.Trim() != "") {
+                retornaArray[k] = author;
+                k++;
+            }
+        }
+        _vars = k;
+        for (int i = 0; i < retornaArray.Length; i++) {
+            if (retornaArray[i] == ("->")) {//explit -> 
+                retornaArray[i] = null;
+            }
+        }
+        return retornaArray;
+    }
+    
+    
+    private Tuple<string[], string, string[]> GetLinesFromTable(TextAsset textAsset) {
+        var tableLines = Regex.Split(textAsset.text, "\r\n|\r|\n");
+        var expressao = tableLines[0]; var numeroLinhasTabela = tableLines[1];
+        var vetorResposta = Regex.Split(tableLines[tableLines.Length - 1].ToUpper(), @"\s+");
+        var linhasTabela = new string[(int) Math.Pow(2,int.Parse(numeroLinhasTabela))];
+
+        for (var i = 2; i <  (int) Math.Pow(2,int.Parse(numeroLinhasTabela)) + 2; i++) {
+            //Debug.Log("linha index " + i + " " +  tableLines[i]);
+            linhasTabela[i - 2] = tableLines[i];
+        }
+        
+        return new Tuple<string[], string, string[]>(linhasTabela, expressao, vetorResposta);
+    }
+
+
+    private Tuple<string, string> getRespostaJogador() {
+        var playerLinha = "";
+        foreach (var validSwitch in _switchesValidos) {
+            if (validSwitch.variavelRef.text != "Resposta") playerLinha += validSwitch.textoTv.text;
+        }
+        var playerResposta = switchVector[4].textoTv.text;
+        
+        return new Tuple<string, string>(playerLinha, playerResposta);
+    }
+    
+    
+    private IEnumerator AnimEvent(string instrucao) { //seta animações tempos etc
+        foreach (var @switch in _switchesValidos) {
+            @switch.ConjuntoSwitchAction(instrucao, true, false);
+        }
+        
+        yield return new WaitForSeconds(2);
+        
+        foreach (var @switch in _switchesValidos) {
+            @switch.ConjuntoSwitchAction(instrucao, false, true);
+        }
+        
+    }
+
+    private void ReiniciaSwitches() {
+        foreach (var @switch in _switchesValidos.Where(@switch => @switch.GetComponent<Animator>().GetBool("on"))) {
+            @switch.GetComponent<Animator>().SetBool("on", false);
+            @switch.textoTv.text = "F";
+        }
+    }
+    public void EnviarResposta() {
+        var respostaJogador = getRespostaJogador();
+        
+        if (_logRespostas.Contains(respostaJogador))  EventSystem.current.SetSelectedGameObject(null); //Linha que já foi respondida
+        
+        if (_tuplaDeLinhasRespostas.Contains(respostaJogador)) { //Resposta Correta
+            logLinhas.text += string.Format("{0,-2} - {1,-2} -> {2}\n", (_logRespostas.Count + 1), respostaJogador.Item1, respostaJogador.Item2);
+            StartCoroutine(AnimEvent("greenScreen"));
+            _logRespostas.Add(respostaJogador);
+            ReiniciaSwitches();
+            EventSystem.current.SetSelectedGameObject(null);
+            return;
+        }
+        
+        StartCoroutine(AnimEvent("redScreen")); //Resposta Errada
+        ReiniciaSwitches();
+        
+        Debug.Log("Log de respostas");
+        foreach (var respostas in _logRespostas) {
+            Debug.Log(respostas);
+        }
+
+        EventSystem.current.SetSelectedGameObject(null); 
+        /*Faz com que nada mais esteja selecionado pelo
+         *event system, serve para perder o selest do botao*/
     }
 }
